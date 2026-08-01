6f865fed96267577768424e0111cd9be6f341ff5
hadnu <andre_ataide@proton.me>
2026-08-01 21:29:57 +0100

    fix: remove unused navigate import in Builder.svelte

--- frontend/src/lib/views/Builder.svelte
+++ frontend/src/lib/views/Builder.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { navigate } from '../router.svelte'
   import { contentError, loadContent } from '../content.svelte'
   import {
     STEPS,
